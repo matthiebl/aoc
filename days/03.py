d0c97159cd7a242ec4ee958514fd2f91b5601039
Matthew Hiebl <87024986+matthiebl@users.noreply.github.com>
2023-12-03 17:07:39 +1100

    Clean up day 3 slightly

--- days/03.py
+++ days/03.py
@@ -12,56 +12,49 @@ def main(file: str) -> None:
     p1 = []
     V = set()
 
-    def find_num(x, y):
-        if (x, y) in V:
+    def add_num(x, y):
+        if x < 0 or y < 0 or x >= len(data[0]) or y >= len(data):
             return
-        if x < 0 or y < 0:
+        if data[y][x] not in '1234567890':
             return
-        if x >= len(data[0]) or y >= len(data):
-            return
+        # here coord is valid number
 
-        if data[y][x] not in '0987654321':
-            return
-        while x > 0 and data[y][x] in '0987654321':
+        while x > 0 and data[y][x] in '1234567890':
             x -= 1
-        if data[y][x] not in '0987654321':
+        if data[y][x] not in '1234567890':
             x += 1
+        # at most significant number, so check if found else add it
         if (x, y) in V:
             return
-
-        n = 0
         V.add((x, y))
-        while x < len(data[0]):
-            c = data[y][x]
-            if c not in '0987654321':
-                p1.append(n)
-                return
-            n *= 10
-            n += int(c)
-            x += 1
-        p1.append(n)
 
-    p2 = []
+        n = ''
+        while x < len(data[0]) and data[y][x] in '1234567890':
+            n += data[y][x]
+            x += 1
+        p1.append(int(n))
+
+    p2 = 0
     for y, row in enumerate(data):
         for x, c in enumerate(row):
             if c in '1234567890.':
                 continue
+            # only care about parts
             l1 = len(p1)
-            find_num(x - 1, y - 1)
-            find_num(x - 1, y)
-            find_num(x - 1, y + 1)
-            find_num(x, y - 1)
-            find_num(x, y + 1)
-            find_num(x + 1, y - 1)
-            find_num(x + 1, y)
-            find_num(x + 1, y + 1)
+            add_num(x - 1, y - 1)
+            add_num(x - 1, y)
+            add_num(x - 1, y + 1)
+            add_num(x, y - 1)
+            add_num(x, y + 1)
+            add_num(x + 1, y - 1)
+            add_num(x + 1, y)
+            add_num(x + 1, y + 1)
             l2 = len(p1)
             if c == '*' and l2 - l1 == 2:
-                a = p1[-1]
-                b = p1[-2]
-                p2.append(a * b)
-    print(sum(p1))
-    print(sum(p2))
+                p2 += p1[-1] * p1[-2]
+
+    print('p1=' + str(sum(p1)))
+    print(f'{p2=}')
 
 
 if __name__ == '__main__':
