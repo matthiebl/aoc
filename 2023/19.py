1897ab07a7c17c73ef80ec81b76d2755b91dcf7b
Matthew Hiebl <87024986+matthiebl@users.noreply.github.com>
2023-12-21 22:21:29 +1100

    Add some description logic

--- 2023/19.py
+++ 2023/19.py
@@ -3,6 +3,27 @@
 import aocutils as u
 from sys import argv
 
+"""
+--- Day 19: Aplenty ---
+
+This ended up being easier than I made it out to be.
+
+For some reason, when solving the problem, I had assumed there would be overlapping
+sections when I got an accepted part range in part 2 that I would have to remove.
+
+I spent a long time making a function to union and manage the 4D ranges that I had
+somehow produced through what I assume must have been a bug. Once I had finally got
+this function working, I watched hyper-neutrino's video solution, which ended up
+being a slightly modified version of my recursive function, that didn't bother with
+any of the pointless range overlaps.
+
+I then tried to refactor my function, and excluding the pointless overlap logic.
+This ended up solving all of my issues and gave me the correct answer.
+
+So went from having the correct answer in my grasp only a short time after the
+puzzle's release, to pointlessly overcomplicating the problem.
+"""
+
 Workflows = dict[str, tuple[list[tuple[str, str, int, str]], str]]
 
 OPS = {
@@ -25,7 +46,7 @@ def is_accepted(workflows: Workflows, ratings: dict[str, int], name: str = 'in')
 
 def count_accepted(workflows: Workflows, ranges: dict[str, u.Coord], name: str = 'in'):
     if any(b - a <= 0 for a, b in ranges.values()):
-        return
+        return 0
     if name == 'R':
         return 0
     if name == 'A':
