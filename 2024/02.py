d4515aa44f5086e0c323d89340695d3a5c7a26c7
Matthew Hiebl <87024986+matthiebl@users.noreply.github.com>
2024-12-02 15:35:24 +1000

    Tidy 2024 day 2

--- 2024/02.py
+++ 2024/02.py
@@ -5,6 +5,11 @@ from sys import argv
 import aocutils as u
 
 
+def report_is_safe(report: list[int]) -> bool:
+    diffs = [report[i] - report[i+1] for i in range(len(report) - 1)]
+    return all(1 <= abs(x) <= 3 for x in diffs) and (all(x < 0 for x in diffs) or all(x > 0 for x in diffs))
+
+
 def main(file: str) -> None:
     print('Day 02')
 
@@ -13,17 +18,13 @@ def main(file: str) -> None:
     p1 = 0
     p2 = 0
     for report in data:
-        diffs = [report[i] - report[i+1] for i in range(len(report) - 1)]
-        if all(1 <= abs(x) <= 3 for x in diffs) and (all(x < 0 for x in diffs) or all(x > 0 for x in diffs)):
+        if report_is_safe(report):
             p1 += 1
+        # Remove number one by one and check
         for i in range(len(report)):
-            new_report = report[:i] + report[i+1:]
-            diffs = [new_report[i] - new_report[i+1] for i in range(len(new_report) - 1)]
-            if all(1 <= abs(x) <= 3 for x in diffs) and (all(x < 0 for x in diffs) or all(x > 0 for x in diffs)):
+            if report_is_safe(report[:i] + report[i+1:]):
                 p2 += 1
                 break
-
-    print(len(data))
     print(f'{p1=}')
     print(f'{p2=}')
 
