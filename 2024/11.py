b087d7a06784307736ce57d3d9ad278a539a414b
Matthew Hiebl <87024986+matthiebl@users.noreply.github.com>
2024-12-11 18:15:58 +1100

    Refactor 2024 day 11

--- 2024/11.py
+++ 2024/11.py
@@ -10,21 +10,17 @@ stones = list(nums(raw))
 def split_stones_count(stone: int, time: int):
     if time == 0:
         return 1
+    if stone == 0:
+        return split_stones_count(1, time - 1)
     stone_str = str(stone)
     stone_str_len = len(stone_str)
-    total = 0
-    if stone == 0:
-        total += split_stones_count(1, time - 1)
-    elif stone_str_len % 2 == 0:
-        total += split_stones_count(int(stone_str[:stone_str_len // 2]), time - 1)
-        total += split_stones_count(int(stone_str[stone_str_len // 2:]), time - 1)
-    else:
-        total += split_stones_count(stone * 2024, time - 1)
-    return total
+    if stone_str_len % 2 == 0:
+        return split_stones_count(int(stone_str[:stone_str_len // 2]), time - 1) + split_stones_count(int(stone_str[stone_str_len // 2:]), time - 1)
+    return split_stones_count(stone * 2024, time - 1)
 
 
-p1 = sum(split_stones_count(s, 25) for s in stones)
+p1 = sum(split_stones_count(stone, 25) for stone in stones)
 print(p1)
 
-p2 = sum(split_stones_count(s, 75) for s in stones)
+p2 = sum(split_stones_count(stone, 75) for stone in stones)
 print(p2)
