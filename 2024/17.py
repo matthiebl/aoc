f53b61c07130975a1fb2bd5055c3f6cf8b75c284
Matthew Hiebl <87024986+matthiebl@users.noreply.github.com>
2024-12-18 22:34:17 +1100

    Minor tidy for day 17

--- 2024/17.py
+++ 2024/17.py
@@ -17,7 +17,7 @@ def run(A: int, B: int = 0, C: int = 0):
     while ip < len(code) - 1:
         opcode, operand = code[ip], code[ip + 1]
         comb = A if operand == 4 else B if operand == 5 else C if operand == 6 else operand
-        
+
         A = A >> comb if opcode == 0 else A
         B = B ^ operand if opcode == 1 else comb % 8 if opcode == 2 else B ^ C if opcode == 4 else A >> comb if opcode == 6 else B
         C = A >> comb if opcode == 7 else C
@@ -35,7 +35,7 @@ def reverse_engineer(i, start):
     if i == -1:
         return int(start, 8)
     total = [reverse_engineer(i - 1, start + n) for n in "01234567" if run(int(start + n, 8))[0] == str(code[i])]
-    return min(total) if total else 8 ** 17
+    return min(total) if total else float("inf")
 
 
 p2 = reverse_engineer(len(code) - 1, "0o")
