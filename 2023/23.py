5a4a99123aa6f4db0c5d3b3c66b471759348e496
Matthew Hiebl <87024986+matthiebl@users.noreply.github.com>
2025-01-29 20:30:13 +1100

    Update 2023 day 23

--- 2023/23.py
+++ 2023/23.py
@@ -1,79 +1,75 @@
-#!/usr/bin/env python3.12
+"""
+--- Day 23: A Long Walk ---
+https://adventofcode.com/2023/day/23
+"""
 
-import aocutils as u
-from sys import argv
+from collections import deque
 
-DIRS = {
-    '^': [(0, -1)],
-    '>': [(1, 0)],
-    'v': [(0, 1)],
-    '<': [(-1, 0)],
-    '.': u.DXY,
-}
+from utils import *
 
+args = parse_args(year=2023, day=23)
+raw = get_input(args.filename, year=2023, day=23)
 
-def main(file: str) -> None:
-    print('Day 23')
+grid = [list(line) for line in raw.splitlines()]
+R, C = len(grid), len(grid[0])
+start = (0, 1)
+end = (R - 1, C - 2)
 
-    grid = u.input_as_lines(file)
-    start = (1, 0)
-    finish = (len(grid[0]) - 2, len(grid) - 1)
+directional = {">": (0, 1), "v": (1, 0), "<": (0, -1), "^": (-1, 0)}
 
-    G = u.graph_from_grid(grid, '.^>v<')
+intersections = set([start, end])
+for (r, c), v, _ in enumerate_grid(grid, skip="#"):
+    if len([n for _, n in neighbours(grid, r, c) if n != "#"]) > 2:
+        intersections.add((r, c))
 
-    nodes = [n for n in G if len(G[n]) > 2 or len(G[n]) == 1]
-
-    G1 = {n: {} for n in nodes}
-    G2 = {n: {} for n in nodes}
-
-    def adjacency_list(n, is_p1):
-        S = [(n, 0)]
-        V = {n}
-        while S:
-            (x, y), d = S.pop()
-
-            if d > 0 and (x, y) in nodes:
-                if is_p1:
-                    G1[n][(x, y)] = d
-                else:
-                    G2[n][(x, y)] = d
-                continue
-
-            dirs = DIRS[grid[y][x]] if is_p1 else u.DXY
-            for dx, dy in dirs:
-                nx, ny = x + dx, y + dy
-                if u.in_grid(grid, ny, nx) and grid[ny][nx] != '#' and (nx, ny) not in V:
-                    S.append(((nx, ny), d + 1))
-                    V.add((nx, ny))
-
-    for n in nodes:
-        adjacency_list(n, True)
-        adjacency_list(n, False)
-
-    def solve(G):
-        seen = set()
-
-        def longest_path(n, dist):
-            if n == finish:
-                return dist
-
-            longest = -float('inf')
-            seen.add(n)
-            for nn, nd in G[n].items():
-                if nn not in seen:
-                    longest = max(longest, longest_path(nn, dist + nd))
-            seen.remove(n)
-
-            return longest
-
-        return longest_path(start, 0)
-
-    p1 = solve(G1)
-    print(f'{p1=}')
-    p2 = solve(G2)
-    print(f'{p2=}')
+directional_graph = {n: [] for n in intersections}
+graph = {n: [] for n in intersections}
 
 
-if __name__ == '__main__':
-    file = argv[1] if len(argv) >= 2 else '23.in'
-    main(file)
+def intersection_search(pos: tuple):
+    visited = set()
+    queue = deque([(0, pos, False)])
+    while queue:
+        d, (r, c), climbed = queue.popleft()
+        if (r, c) in visited:
+            continue
+        visited.add((r, c))
+
+        if (r, c) in intersections and (r, c) != pos:
+            if not climbed:
+                directional_graph[pos].append((d, (r, c)))
+            graph[pos].append((d, (r, c)))
+            continue
+
+        for (nr, nc), n in neighbours(grid, r, c):
+            if n in directional and (nr - r, nc - c) != directional[n]:
+                queue.append((d + 1, (nr, nc), True))
+            elif n != "#":
+                queue.append((d + 1, (nr, nc), climbed))
+
+
+def dfs(graph: dict, visited: set = set(), pos: tuple = start, dist: int = 0):
+    if pos == end:
+        return dist
+
+    longest = 0
+    visited.add(pos)
+    for d, adj in graph[pos]:
+        if adj in visited:
+            continue
+        longest = max(longest, dfs(graph, visited, adj, dist + d))
+    visited.remove(pos)
+    return longest
+
+
+for intersection in intersections:
+    intersection_search(intersection)
+
+p1 = dfs(directional_graph)
+print(p1)
+
+p2 = dfs(graph)
+print(p2)
+
+if args.test:
+    args.tester(p1, p2)
