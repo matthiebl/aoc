1fd689192f2730f54aa6362d09ee59df79f535e7
Matthew Hiebl <87024986+matthiebl@users.noreply.github.com>
2025-01-31 00:41:40 +1100

    Update 2023 day 20

--- 2023/20.py
+++ 2023/20.py
@@ -1,116 +1,109 @@
-#!/usr/bin/env python3.12
+"""
+--- Day 20: Pulse Propagation ---
+https://adventofcode.com/2023/day/20
+"""
 
-import aocutils as u
-from sys import argv
+from collections import deque
+from enum import Enum
 from math import lcm
 
-LOW = 'LOW'
-HIGH = 'HIGH'
-STOP = 'STOP'
+from utils import *
+
+args = parse_args(year=2023, day=20)
+raw = get_input(args.filename, year=2023, day=20)
+
+
+class Pulse(Enum):
+    LOW = 0
+    HIGH = 1
+    STOP = 2
+
 
 modules = {}
+for info in raw.splitlines():
+    name, rest = info.split(" -> ")
+
+    props = {
+        "type": name[0],
+        "name": name[1:] if name[0] != "b" else name,
+        "outputs": rest.split(", "),
+    }
+
+    if "rx" in props["outputs"]:
+        rx_input = props["name"]
+    if props["type"] == "&":
+        props["memory"] = {}
+    elif props["type"] == "%":
+        props["on"] = False
+
+    modules[props["name"]] = props
+
+rx_input_inputs = set()
+for module in modules:
+    for output in modules[module]["outputs"]:
+        if output == rx_input:
+            rx_input_inputs.add(module)
+        if output in modules and modules[output]["type"] == "&":
+            modules[output]["memory"][module] = Pulse.LOW
+
 signals = {
-    LOW: 0,
-    HIGH: 0,
+    Pulse.LOW: 0,
+    Pulse.HIGH: 0,
 }
 
 
 def reset_modules():
     for props in modules.values():
-        if props['type'] == '%':
-            props['on'] = False
-        elif props['type'] == '&':
-            for module in props['memory']:
-                props['memory'][module] = LOW
-    signals[LOW] = 0
-    signals[HIGH] = 0
+        if props["type"] == "%":
+            props["on"] = False
+        elif props["type"] == "&":
+            props["memory"] = {module: Pulse.LOW for module in props["memory"]}
+    signals[Pulse.LOW] = 0
+    signals[Pulse.HIGH] = 0
 
 
 def push_button(check=None):
-    Q = [('broadcaster', LOW, None)]
-    while Q:
-        name, recv, last = Q.pop(0)
-        # print(name, recv, last, Q)
-        if name == check and recv == LOW:
+    queue = deque([("broadcaster", Pulse.LOW, None)])
+    while queue:
+        name, recv, last = queue.popleft()
+        if name == check and recv == Pulse.LOW:
             return True
         props = modules[name]
-
         signals[recv] += 1
 
         send = recv
-        if props['type'] == '&':
-            props['memory'][last] = recv
-            send = LOW if all(
-                sig == HIGH for sig in props['memory'].values()) else HIGH
-        elif props['type'] == '%':
-            if recv == HIGH:
+        if props["type"] == "&":
+            props["memory"][last] = recv
+            send = Pulse.LOW if all(sig == Pulse.HIGH for sig in props["memory"].values()) else Pulse.HIGH
+        elif props["type"] == "%":
+            if recv == Pulse.HIGH:
                 continue
-            props['on'] = not props['on']
-            send = HIGH if props['on'] else LOW
+            props["on"] = not props["on"]
+            send = Pulse.HIGH if props["on"] else Pulse.LOW
 
-        for nxt in props['outputs']:
+        for nxt in props["outputs"]:
             if nxt not in modules:
                 signals[send] += 1
                 continue
-            Q.append((nxt, send, name))
+            queue.append((nxt, send, name))
     return False
 
 
-def main(file: str) -> None:
-    print('Day 20')
+for _ in range(1000):
+    push_button()
+p1 = mul(signals.values())
+print(p1)
 
-    data = u.input_as_lines(file)
+times = []
+for input in rx_input_inputs:
+    reset_modules()
+    for t in range(10**9):
+        if push_button(input):
+            times.append(t + 1)
+            break
 
-    for info in data:
-        name, rest = info.split(' -> ')
-        outputs = rest.split(', ')
+p2 = lcm(*times)
+print(p2)
 
-        props = {
-            'type': name[0],
-            'name': name[1:] if name[0] != 'b' else name,
-            'outputs': outputs,
-        }
-
-        if props['type'] == '&':
-            props['memory'] = {}
-        elif props['type'] == '%':
-            props['on'] = False
-
-        modules[props['name']] = props
-
-    nodes = set()
-    edges = []
-    rg_inputs = set()
-    for module in modules:
-        nodes.add((module, modules[module]['type']))
-        for output in modules[module]['outputs']:
-            nodes.add((output, ''))
-            edges.append((module, output))
-            if output == 'rg':
-                rg_inputs.add(module)
-            if output in modules and modules[output]['type'] == '&':
-                modules[output]['memory'][module] = LOW
-
-    for _ in range(1000):
-        push_button()
-    p1 = u.mul(signals.values())
-    print(f'{p1=}')
-
-    times = []
-    for check in rg_inputs:
-        reset_modules()
-        t = 0
-        while True:
-            t += 1
-            low = push_button(check)
-            if low:
-                times.append(t)
-                break
-
-    p2 = lcm(*times)
-    print(f'{p2=}')
-
-
-if __name__ == '__main__':
-    file = argv[1] if len(argv) >= 2 else '20.in'
-    main(file)
+if args.test:
+    args.tester(p1, p2)
