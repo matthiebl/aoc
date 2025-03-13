3c8b1f6e76c73575e467a12603c99747730431b8
Matthew Hiebl <87024986+matthiebl@users.noreply.github.com>
2025-03-13 23:49:04 +1100

    Update 2021 day 7

--- 2021/07.py
+++ 2021/07.py
@@ -1,28 +1,29 @@
-#!/usr/bin/env python3.12
+"""
+--- Day 7: The Treachery of Whales ---
+https://adventofcode.com/2021/day/7
+"""
 
-import aocutils as u
-from sys import argv
+from utils import *
 
+args = parse_args(year=2021, day=7)
+raw = get_input(args.filename, year=2021, day=7)
 
-def main(file: str) -> None:
-    print('Day 07')
+crabs = list(nums(raw))
 
-    crabs = u.map_int(u.read_input(file).split(','))
+p1 = float("inf")
+p2 = float("inf")
 
-    p1 = 10 ** 10
-    p2 = 10 ** 10
-    for x in range(min(crabs), max(crabs) + 1):
-        a, b = 0, 0
-        for crab in crabs:
-            n = abs(x - crab)
-            a += n
-            b += (n * (n + 1)) // 2
-        p1 = min(p1, a)
-        p2 = min(p2, b)
-    print(f'{p1=}')
-    print(f'{p2=}')
+for x in range(min(crabs), max(crabs) + 1):
+    a, b = 0, 0
+    for crab in crabs:
+        n = abs(x - crab)
+        a += n
+        b += (n * (n + 1)) // 2
+    p1 = min(p1, a)
+    p2 = min(p2, b)
 
+print(p1)
+print(p2)
 
-if __name__ == '__main__':
-    file = argv[1] if len(argv) >= 2 else '07.in'
-    main(file)
+if args.test:
+    args.tester(p1, p2)
