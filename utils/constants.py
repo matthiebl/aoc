369fda9621593f108ef58ac325219cae96e4f175
--global <87024986+matthiebl@users.noreply.github.com>
2025-12-03 22:51:56 +1100

    remove old files

--- utils/constants.py
+++
@@ -1,8 +0,0 @@
-
-class Constants:
-    input_file_ext: str = ".in"
-    example_file_ext: str = ".ex"
-    input_file: str = "{year}/{day}" + input_file_ext
-    example_file: str = "{year}/{day}" + example_file_ext
-    script_file: str = "{day}.py"
-    input_file_re = r"\d{4}\/\d{2}.in"
