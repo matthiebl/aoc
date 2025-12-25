e25524563d570ae84c8ac8e15c4387650f8302b1
--global <87024986+matthiebl@users.noreply.github.com>
2025-12-25 15:34:59 +1100

    solve 2025 day 12

---
+++ src/advent/solutions/year_2025/day_12.py
@@ -0,0 +1,32 @@
+"""
+--- Day 12: Christmas Tree Farm ---
+https://adventofcode.com/2025/day/12
+"""
+
+from advent.core import Solver
+
+
+class Day12(Solver):
+    """Solution for day 12."""
+
+    def prepare(self):
+        [*presents, orders] = self.input.sections()
+        self.store.present_size = [p.count("#") for p in presents]
+        self.store.orders = [list(self.input.nums(order)) for order in orders.lines()]
+
+    def part1(self) -> int:
+        """Solve part 1."""
+        fit = 0
+        for order in self.store.orders:
+            [w, h, *n_shapes] = order
+            area = w * h
+            used = sum(size * n for size, n in zip(self.store.present_size, n_shapes))
+            if used <= area:
+                fit += 1
+        return fit
+
+    def part2(self) -> int:
+        """Solve part 2."""
+        lines = self.input.lines()
+        # TODO: Implement solution
+        return 0
